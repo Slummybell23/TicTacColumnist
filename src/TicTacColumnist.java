81f744764605fc14d149f7a311423d07df73be9b
Nang Bui <nangbui3010@gmail.com>
2024-11-20 14:09:36 -0600

    John's work on checkWinner and getColumn. Adding with dev's code

--- src/TicTacColumnist.java
+++ src/TicTacColumnist.java
@@ -1,6 +1,59 @@
 public class TicTacColumnist implements ITicTacColumnist
 {
     private char[][] board = ITicTacColumnist.board;
+    private char curPlayer = ITicTacColumnist.curPlayer;
+    private int numMoves = ITicTacColumnist.numMoves;
+    private  char winner = ITicTacColumnist.winner;
+
+    @Override
+    public void startGame()
+    {
+        board = new char[3][3];
+        curPlayer = 'O';
+        numMoves = 0;
+        winner = '*';
+    }
+
+    @Override
+    public boolean validateInput(int col, int row)
+    {
+        //Checks if out of bounds
+        if(row >= board.length || row < 0 || col >= board[row].length || col < 0)
+            return false;
+
+        //Checks if space occupied
+        if(board[row][col] == 'X' || board[row][col] == 'O')
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public void makeMove(int col, int row)
+    {
+        board[row][col] = curPlayer;
+
+        //Flips player
+        if(curPlayer == 'O')
+            curPlayer = 'X';
+        else if(curPlayer == 'X')
+            curPlayer = 'O';
+
+        numMoves++;
+    }
+
+    @Override
+    public int gameStatus()
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean isBoardFull()
+    {
+        return false;
+    }
+    
     public int checkWinner()
     {
         for(int i = 0; i < 3; i++){
@@ -25,4 +78,6 @@ public class TicTacColumnist implements ITicTacColumnist
         }
         return col;
     }
+
+
 }
