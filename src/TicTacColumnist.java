4a7ad54ede654aa303b2569e30e933826b3e8548
cachesalyers <cachesalyers@gmail.com>
2024-11-20 12:04:13 -0600

    Implimented methods.

--- src/TicTacColumnist.java
+++ src/TicTacColumnist.java
@@ -1,9 +1,48 @@
 public class TicTacColumnist implements ITicTacColumnist
 {
     private char[][] board = ITicTacColumnist.board;
+    private char curPlayer = ITicTacColumnist.curPlayer;
+    private int numMoves = ITicTacColumnist.numMoves;
+    private  char winner = ITicTacColumnist.winner;
 
+    @Override
+    public void startGame()
+    {
+    }
 
+    @Override
+    public int getColumn()
+    {
+        return 0;
+    }
 
+    @Override
+    public boolean validateInput(int col, int row)
+    {
+        return false;
+    }
 
+    @Override
+    public void makeMove(int col, int row)
+    {
 
+    }
+
+    @Override
+    public int gameStatus()
+    {
+        return 0;
+    }
+
+    @Override
+    public int checkWinner()
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean isBoardFull()
+    {
+        return false;
+    }
 }
